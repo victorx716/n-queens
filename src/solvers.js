26f0f819a55bd7b91b97ea23aa62002f11e2bf7b
victorx716 <victorliu716@gmail.com>
2019-01-12 15:28:03 -0800

    Implement up to findNRooks
    Need to refactor helper functions in Board.js to utilize .get().

--- src/solvers.js
+++ src/solvers.js
@@ -15,15 +15,30 @@
 
 
 
-window.findNRooksSolution = function(n) {
-  var solution = undefined; //fixme
+window.findNRooksSolution = function (n) {
+  var solution;
+  var board = new Board({ n: n })
+  var rooksRemaining = board.get('n');
 
+  var rooks = function (board, r, c) {
+    if (rooksRemaining === 0) {
+      return board;
+    }
+    board.togglePiece(r, c);
+    rooksRemaining--;
+    rooks(board, r + 1, c + 1);
+  };
+
+  rooks(board, 0, 0);
+
+  solution = board.rows();
   console.log('Single solution for ' + n + ' rooks:', JSON.stringify(solution));
   return solution;
 };
 
+
 // return the number of nxn chessboards that exist, with n rooks placed such that none of them can attack each other
-window.countNRooksSolutions = function(n) {
+window.countNRooksSolutions = function (n) {
   var solutionCount = undefined; //fixme
 
   console.log('Number of solutions for ' + n + ' rooks:', solutionCount);
@@ -31,7 +46,7 @@ window.countNRooksSolutions = function(n) {
 };
 
 // return a matrix (an array of arrays) representing a single nxn chessboard, with n queens placed such that none of them can attack each other
-window.findNQueensSolution = function(n) {
+window.findNQueensSolution = function (n) {
   var solution = undefined; //fixme
 
   console.log('Single solution for ' + n + ' queens:', JSON.stringify(solution));
@@ -39,7 +54,7 @@ window.findNQueensSolution = function(n) {
 };
 
 // return the number of nxn chessboards that exist, with n queens placed such that none of them can attack each other
-window.countNQueensSolutions = function(n) {
+window.countNQueensSolutions = function (n) {
   var solutionCount = undefined; //fixme
 
   console.log('Number of solutions for ' + n + ' queens:', solutionCount);
